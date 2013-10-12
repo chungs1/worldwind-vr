3c601d59eee57733291f8df004440a153d027b72
Sophia Chung <chungs1@berkeley.edu>
2013-10-12 02:47:48 -0700

    leap stuff

---
+++ src/leap/SubstituteGame.java
@@ -0,0 +1,14 @@
+package leap;
+
+import com.tuohy.worldwindvr.input.VRFlyView;
+
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.render.DrawContext;
+
+public class SubstituteGame {
+	DrawContext dc;
+	Position position;
+	Double elevation;
+	
+	VRFlyView view = new VRFlyView();
+}
